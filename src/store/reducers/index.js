8858de2df1b6cac01064637e8f5b492b2dc9c2d9
jimynu <kim@bylr.ch>
2017-12-06 09:07:27 +0100

    actions & reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import { bathReducer } from './bath';
 import { userReducer } from './user';
+import { lexemeReducer } from './lexemes';
 
 
 const reducers = combineReducers({
   bath: bathReducer,
   user: userReducer,
+  lexemes: lexemeReducer,
 });
 
 export default reducers;
